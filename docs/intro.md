dc826024acf4ee873725c52aff6ca0ac1879440a
nghinv <nghinv@inpirelab.io>
2022-10-08 11:05:36 +0700

    feat: init website

---
+++ docs/intro.md
@@ -0,0 +1,17 @@
+---
+sidebar_position: 1
+---
+
+# Introduction
+
+## What is Nix Aptos Wallet?
+
+Browser Extension acts as your portal to the Web3 world. A friendly crypto wallet
+
+**Nix Aptos Wallet** makes it safe & easy for you to store, buy, send, receive, swap tokens and collect NFTs on the Aptos blockchain.
+
+## Install Chrome Extensions
+
+1. Go to <https://chrome.google.com/webstore/category/extensions> and pick an extension.
+2. Click `Add to Chrome` button.
+3. Click `Install` button in the Extensions Manager.
---
+++ docusaurus.config.js
@@ -0,0 +1,89 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Nix Aptos Wallet",
+  tagline:
+    "The Nix Aptos Wallet Browser Extension acts as your portal to the Web3 world",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+  plugins: ["docusaurus-plugin-sass"],
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/nixjs/nix-aptos-wallet",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/nixjs/nix-aptos-wallet",
+        },
+        theme: {
+          customCss: [require.resolve("./src/css/custom.scss")],
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Nix Aptos Wallet",
+        logo: {
+          alt: "Nix Aptos Wallet Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
+          },
+          {
+            href: "https://github.com/nixjs/nix-aptos-wallet",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        copyright: `Created with 💖 by <a target="_blank" rel="noopener noreferrer" href="https://github.com/nixjs/grpc-client-toolkit">@nixjs</a>. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
